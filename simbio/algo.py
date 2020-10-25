bc3436dfcf1226565f9f36cfaf6ba91ee1c6bc8b
Sebastián Schiavinato <seba.schiavinato@gmail.com>
2020-10-25 19:23:09 -0300

    First iteration

--- simbio/algo.py
+++ simbio/algo.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from typing import Tuple, Union
 
 import numpy as np
+import pyabc
 
+from .compartments import Compartment
 from .parameters import BaseParameter, Parameter
 from .simulator import Simulator
 from .solvers.core import BaseSolver
@@ -78,3 +80,66 @@ class Scanner:
     def __iter__(self):
         for scan_value in self.scan_values:
             yield self.simulator.create_solver(values=scan_value)
+
+
+def infer_abc(
+    model: Compartment,
+    experiment_data: Tuple[np.ndarray, np.ndarray],
+    db_path="sqlite:///test.db",
+    debug=False,
+):
+    """
+    pyABC inference of a model and the experimental data. The experimental data in a form of (t, y) tuple.
+    You can provide the path of the sqlite DB to save the historical data of pyABC and if flag for debugging
+    """
+
+    # Get the running time and the experimenta data
+    run_time, exp_data = experiment_data
+
+    # Assuming will maintain the order!
+    initial_values_per_specie = {}
+    initial_value = exp_data[:, 0]  # First row of data
+
+    # We get the relative name of the species (with the container) which is in use by the simulator
+    cell_species = model._in_reaction_species_names
+    for i, specie_nane in enumerate(cell_species):
+        initial_values_per_specie[specie_nane] = initial_value[i]
+
+    if debug:
+        print("Initial values for species: {}".format(initial_values_per_specie))
+
+    # We get the relative names (with the container name) for the model,
+    # which is the name in use for the simulator
+    cell_parameters = model._in_reaction_parameter_names
+
+    prior_param_dict = {}
+    for param in cell_parameters:
+        prior_param_dict[param.name] = pyabc.RV("uniform", 0, 1)
+
+    if debug:
+        print("Parameters distribution: {}".format(prior_param_dict))
+
+    def distance(exp_data, simulation):
+        return np.absolute(exp_data["y"] - simulation["y"]).sum()
+
+    def abc_model(params):
+        values = {**initial_values_per_specie, **params}
+        sim = Simulator(model, values=values)
+        t, y = sim.run(run_time)
+        return {"y": y}
+
+    abc = pyabc.ABCSMC(
+        models=abc_model,
+        parameter_priors=pyabc.Distribution(**prior_param_dict),
+        distance_function=distance,
+        population_size=50,
+        transitions=pyabc.LocalTransition(k_fraction=0.3),
+        eps=pyabc.MedianEpsilon(500, median_multiplier=0.7),
+    )
+
+    # Where to save the history data
+    h = abc.new(db_path, {"y": exp_data})
+
+    h = abc.run(minimum_epsilon=0.5, max_nr_populations=10)
+
+    return h
