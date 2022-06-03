7bd9feda90a471c4854b7fa9118aea922afb9c18
Mauro Silberberg <maurosilber@gmail.com>
2022-06-03 11:27:17 -0300

    Test references

---
+++ simbio/model/tests/builder/test_reference.py
@@ -0,0 +1,28 @@
+from simbio.model import EmptyCompartment, Parameter, Species, reactions
+from simbio.model._container import Reference
+
+
+class Model(EmptyCompartment):
+    A: Species = 1
+    k: Parameter = 1
+    create_A = reactions.Creation(A=A, rate=k)
+
+
+def test_contents_are_references():
+    assert isinstance(Model.A, Reference)
+    assert isinstance(Model.k, Reference)
+    assert isinstance(Model.create_A.A, Reference)
+    assert isinstance(Model.create_A.rate, Reference)
+
+
+def test_containers_are_not_references():
+    assert not isinstance(Model, Reference)
+    assert not isinstance(Model.create_A, Reference)
+
+
+def test_resolve_reference():
+    assert Model.A != Model.create_A.A
+    assert Model.k != Model.create_A.rate
+
+    assert Model.A.resolve() == Model.create_A.A.resolve(recursive=True)
+    assert Model.k.resolve() == Model.create_A.rate.resolve(recursive=True)
