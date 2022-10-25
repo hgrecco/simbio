84815ec812cdbbd1177ba0194e88cbb7be829bb6
Mauro Silberberg <maurosilber@gmail.com>
2022-10-25 00:55:37 -0300

    Add tests for resolution of initial conditions

---
+++ src/simbio/compilers/tests/test_species.py
@@ -0,0 +1,33 @@
+from ...components import EmptyCompartment, Species
+from ...reactions.single import Creation
+from ..core import Compiler
+
+
+class Compiler(Compiler):
+    def _build_reaction_ip_rhs(self, full_name: str, reaction) -> callable:
+        raise RuntimeError
+
+
+class Model(EmptyCompartment):
+    A: Species = 1
+    r1 = Creation(A=A, rate=0)
+    r2 = Creation(A=A, rate=0)
+    r3 = Creation(A=3, rate=0)
+
+
+compiler = Compiler(Model)
+
+
+def test_number_of_species():
+    assert len(compiler.species) == 2
+
+
+def test_build_initial_conditions():
+    y0, _ = compiler.build_value_vectors()
+    assert y0.to_dict() == {"A": 1, "r3.A": 3}
+
+    y0, _ = compiler.build_value_vectors({"A": 2, "r3.A": 4})
+    assert y0.to_dict() == {"A": 2, "r3.A": 4}
+
+    y0, _ = compiler.build_value_vectors({Model.A: 2, Model.r3.A: 4})
+    assert y0.to_dict() == {"A": 2, "r3.A": 4}
