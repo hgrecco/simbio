3e99e30dfda3c86539827798b5039f2e1a477264
Mauro Silberberg <maurosilber@gmail.com>
2023-08-11 15:38:55 -0300

    Add test for reactions and fix MichaelisMenten approximations

---
+++ src/simbio/reactions/test_reactions.py
@@ -0,0 +1,18 @@
+import numpy as np
+from pytest import mark
+from simbio.core import Compartment, Simulator
+from simbio.reactions import compound, enzymatic, single
+
+reactions = []
+for mod in (single, compound, enzymatic):
+    for name in dir(mod):
+        value = getattr(mod, name)
+        if isinstance(value, type(Compartment)):
+            reactions.append(value)
+
+
+@mark.parametrize("reaction", reactions)
+def test_reactions(reaction):
+    model = reaction()
+    times = np.linspace(0, 1, 10)
+    Simulator(model).solve(times=times)
