32b34f26d7d495a93bdb89020df61b4d3a6355a7
Mauro Silberberg <maurosilber@gmail.com>
2023-10-05 17:07:47 +0100

    Remove old modules

--- src/simbio/__main__.py
+++
@@ -1,9 +0,0 @@
-import typer
-
-from .cli import models
-
-app = typer.Typer(no_args_is_help=True)
-app.add_typer(models.app, name="model", no_args_is_help=True)
-
-if __name__ == "__main__":
-    app()
