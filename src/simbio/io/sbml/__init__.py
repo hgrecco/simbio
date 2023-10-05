fc1cc8ac50c3eb537b03862361b0d18769a172d8
Mauro Silberberg <maurosilber@gmail.com>
2023-10-05 22:15:29 +0100

    Remove exporter

--- src/simbio/io/sbml/__init__.py
+++ src/simbio/io/sbml/__init__.py
@@ -1,4 +1,3 @@
-from .exporter import SBMLModel
 from .importer import convert_model, parse_model, read_model
 
-__all__ = ["convert_model", "parse_model", "read_model", "SBMLModel"]
+__all__ = ["convert_model", "parse_model", "read_model"]
