0c270f2a187dc4ce348bcea4ee0fe0fd205c4aaf
Mauro Silberberg <maurosilber@gmail.com>
2023-10-19 11:49:36 +0100

    Allow setting different name when loading model from BioModels

--- src/simbio/io/biomodels.py
+++ src/simbio/io/biomodels.py
@@ -3,10 +3,10 @@ import biomodels
 from .sbml import loads
 
 
-def load_model(model_id: str):
+def load_model(model_id: str, *, name: str | None = None):
     omex = biomodels.get_omex(model_id)
     text = omex.master.read_text()
-    model = loads(text)
+    model = loads(text, name=name)
     return model
 
 
